d5d457bc5ad5ec216ed28dcb1dc7a277a2300374
Ldelillo <ldelillo99@gmail.com>
2024-10-16 22:10:44 +0200

    ataque funcional

--- GameLab/Assets/Scripts/ataquemelee.cs
+++ GameLab/Assets/Scripts/ataquemelee.cs
@@ -4,17 +4,17 @@ public class PlayerAttack : MonoBehaviour
 {
     public float attackRange = 0.5f;   // Rango del ataque melee
     public int attackDamage = 10;      // Daño del ataque
-    public LayerMask enemyLayers;      // Capas que representan a los enemigos
 
     public Transform attackPoint;      // El punto desde donde se originará el ataque
-    public Animator animator;          // Controlador de animaciones del personaje
+    //public Animator animator;          // Controlador de animaciones del personaje
 
     public float attackCooldown = 0.5f;   // Tiempo entre ataques
     private float nextAttackTime = 0f;    // Tiempo para el siguiente ataque
 
+    public KeyCode attackKey = KeyCode.Z;
     void Update()
     {
-        if (Time.time >= nextAttackTime && Input.GetKeyDown(KeyCode.Z))
+        if (Time.time >= nextAttackTime && Input.GetKeyDown(attackKey))
         {
             PerformAttack();
             nextAttackTime = Time.time + attackCooldown;
@@ -24,15 +24,17 @@ public class PlayerAttack : MonoBehaviour
     void PerformAttack()
     {
         // Reproduce la animación de ataque
-        animator.SetTrigger("Attack");
+        //animator.SetTrigger("Attack");
 
         // Detectar enemigos en el rango de ataque
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
 
         // Infligir daño a los enemigos detectados
         foreach (Collider2D enemy in hitEnemies)
-        {
+        {   
+            if(enemy.CompareTag("Enemigo")){
             enemy.GetComponent<Damageable>().TakeDamage(attackDamage);
+            }
         }
     }
 
