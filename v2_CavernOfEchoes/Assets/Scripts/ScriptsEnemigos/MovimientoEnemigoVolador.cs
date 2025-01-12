9bfe4c4dc16f88112cccc4375c76857630e92849
Alvaro <alvaro.salamero.garcia@alumnos.upm.es>
2025-01-12 18:29:07 +0100

    Fix: Added new tags so flying enemy detects all players

--- v2_CavernOfEchoes/Assets/Scripts/ScriptsEnemigos/MovimientoEnemigoVolador.cs
+++ v2_CavernOfEchoes/Assets/Scripts/ScriptsEnemigos/MovimientoEnemigoVolador.cs
@@ -110,7 +110,7 @@ public class MovimientoEnemigoVolador : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Arquero") || collision.gameObject.CompareTag("Tanque"))
         {
             // Infligir daño al jugador
             collision.gameObject.GetComponent<Player>().recibirDaño(daño);
